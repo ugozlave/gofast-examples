9da42cc61983d20d63dfa5615c5b91db222e98fb
ugozlave <contact@maxencebar.me>
2025-11-16 10:10:04 +0000

    update to v0.6.0

--- main.go
+++ main.go
@@ -1,88 +1,122 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"slices"
 
 	fast "github.com/ugozlave/gofast"
 	"github.com/ugozlave/gofast/faster"
 )
 
 func main() {
-	app := fast.New(faster.NewAppConfig())
-
-	// logger
-	fast.Log(app, faster.NewFastLogger)
-
-	// controllers
-	fast.Add(app, faster.NewHealthController)
-	fast.Add(app, NewMyController)
-
-	// middleware
-	fast.Use(app, faster.NewLogMiddleware)
-
-	// services
-	fast.Register[IService](app, NewMyService)
+	app := faster.New()
 
 	// config
-	fast.Cfg(app, NewMyConfig)
+	fast.Cfg(app, NewUserConfig)
+
+	// controllers
+	fast.Add(app, NewUserController)
+
+	// middlewares
+	fast.Use(app, NewUserMiddleware)
+
+	// services
+	fast.Register[IUserService](app, NewUserService)
 
 	app.Run()
 }
 
-type MyController struct {
-	service IService
+// config
+
+type UserConfig struct {
+	Users []string `json:"Users"`
 }
 
-func NewMyController(ctx *fast.BuilderContext) *MyController {
-	return &MyController{
-		service: fast.Get[IService](ctx, fast.Scoped),
+func NewUserConfig(_ *fast.BuilderContext) *faster.FastConfig[UserConfig] {
+	var v UserConfig
+	v.Users = []string{"root:root"}
+	return faster.NewConfig(v, "UserSettings")
+}
+
+// controllers
+
+type UserController struct {
+	logger  fast.Logger
+	service IUserService
+}
+
+func NewUserController(ctx *fast.BuilderContext) *UserController {
+	return &UserController{
+		logger:  fast.MustGetLogger[UserController](ctx, fast.Scoped),
+		service: fast.MustGet[IUserService](ctx, fast.Scoped),
 	}
 }
 
-func (c *MyController) Prefix() string {
+func (c *UserController) Prefix() string {
 	return "/my"
 }
 
-func (c *MyController) Routes() http.Handler {
+func (c *UserController) Routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", c.Get)
 	return mux
 }
 
-func (c *MyController) Get(w http.ResponseWriter, r *http.Request) {
-	c.service.DoSomething()
+func (c *UserController) Get(w http.ResponseWriter, r *http.Request) {
+	username := c.service.Get("username")
+	c.logger.Inf("authorized: " + username.(string))
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
-type IService interface {
-	DoSomething()
+// middlewares
+
+type UserMiddleware struct {
+	config  fast.Config[UserConfig]
+	service IUserService
 }
 
-type MyService struct {
-	logger fast.Logger
-	config fast.ConfigProvider[MyConfig]
-}
-
-func NewMyService(ctx *fast.BuilderContext) *MyService {
-	return &MyService{
-		logger: fast.GetLogger[MyService](ctx, fast.Scoped),
-		config: fast.MustGetConfig[MyConfig](ctx, fast.Singleton),
+func NewUserMiddleware(ctx *fast.BuilderContext) *UserMiddleware {
+	return &UserMiddleware{
+		config:  fast.MustGetConfig[UserConfig](ctx, fast.Singleton),
+		service: fast.MustGet[IUserService](ctx, fast.Scoped),
 	}
 }
 
-func (s *MyService) DoSomething() {
-	s.logger.Inf("Hello World")
-	s.logger.Dbg(fmt.Sprintf("Config setting: %s", s.config.Value().Setting))
+func (m *UserMiddleware) Handle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		users := m.config.Value().Users
+		username, password, _ := r.BasicAuth()
+		if !slices.Contains(users, username+":"+password) {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+		m.service.Set("username", username)
+		next.ServeHTTP(w, r)
+	})
 }
 
-type MyConfig struct {
-	Setting string `json:"Setting"`
+// services
+
+type IUserService interface {
+	Get(string) any
+	Set(string, any)
 }
 
-func NewMyConfig(_ *fast.BuilderContext) *faster.Config[MyConfig] {
-	var v MyConfig
-	v.Setting = "default"
-	return faster.NewConfig(v)
+type UserService struct {
+	data map[string]any
+}
+
+func NewUserService(ctx *fast.BuilderContext) *UserService {
+	return &UserService{
+		data: make(map[string]any),
+	}
+}
+
+func (s *UserService) Get(key string) any {
+	return s.data[key]
+}
+
+func (s *UserService) Set(key string, value any) {
+	s.data[key] = value
 }
